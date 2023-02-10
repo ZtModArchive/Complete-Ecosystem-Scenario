823f159e30963ff96cf497f8b3a20b7aedb45135
Apodemus Erectus <bnkho98@hotmail.com>
2023-02-10 22:03:40 +0100

    add more files

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Complete Ecosystem Scenario
+
 [![Validate](https://github.com/ZtModArchive/Complete-Ecosystem-Scenario/actions/workflows/validate.yml/badge.svg)](https://github.com/ZtModArchive/Complete-Ecosystem-Scenario/actions/workflows/validate.yml)
 
 > "_an animal was ...EATEN! (or just attacked : ) would be nice if we could differentiate the failure message depending on whether the eaten event was viewed by a guest or not..._"
---
+++ scenario/scripts/worldcampaign_new.lua
@@ -0,0 +1,22 @@
+include("scenario/scripts/misc.lua")
+include("scenario/scripts/ui.lua")
+include("scenario/scripts/entity.lua")
+include("scenario/scripts/needs.lua")
+include("scenario/scripts/economy.lua")
+evalhugebiome = function()
+end
+
+completeworldcampaignscen4 = function()
+   BFLOG(SYSTRACE, "completeworldcampaignscen2")
+   setuservar("worldcampaignscenario4", "completed")
+   showscenariowin("TheWorld:HugeBiomeSuccessoverview", nil)  
+end
+
+failworldcampaignscen4 = function()
+-- if condition fulfilled, show the alternative message
+   if (guestresponse) ~= nil then
+		showscenariofail("TheWorld:HugeBiomeFailureoverview", "worldcampaignscenario4")
+   else
+		showscenariofail("TheWorld:HugeBiomeFailureWitnessedoverview", "worldcampaignscenario4")
+   end
+end
